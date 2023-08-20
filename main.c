30173612fca23c2602053fc9a1011d1fa874542f
rafastrada <57600424+rafastrada@users.noreply.github.com>
2023-08-20 15:36:34 -0300

    Actualizacion main.c

--- main.c
+++ main.c
@@ -1,10 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "Envio.h"
 
 int main()
 {
     printf("Hello world!\n");
-    printf("Que miras bobo!");
+
+    // TEST
+    Envio prueba;
+    Envio_init(&prueba);
+
+    printf("%s\n%u\n%s\n%s\n%u\n%s\n%s\n%s\n\n",
+           Envio_get_codigo_envio(&prueba),
+           Envio_get_dni_receptor(&prueba),
+           Envio_get_nombre_apellido_receptor(&prueba),
+           Envio_get_domicilio_receptor(&prueba),
+           Envio_get_dni_remitente(&prueba),
+           Envio_get_nombre_apellido_remitente(&prueba),
+           Envio_get_fecha_envio(&prueba),
+           Envio_get_fecha_recepcion(&prueba)
+           );
 
     return 0;
 }
