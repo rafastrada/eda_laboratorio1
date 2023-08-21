9173bb7a2ba3be6dce25f6cee05615a4ed99a2a9
aezequiel <aezequiel096@gmail.com>
2023-08-20 21:22:59 -0300

    main con memorizacion previa

--- main.c
+++ main.c
@@ -23,3 +23,47 @@ int main()
 
     return 0;
 }
+
+//memorizacion previa
+
+/*int memorizacion_previa(Lista *lista, int *cant_repetidos, int *cargados, int tipo_orden){
+    FILE *fichero;
+    int resultado_alta = 1, repetidos = 0, indice = 0;
+    Envio nuevo;
+    fichero = fopen("Vendedores.txt","r");
+    if (fichero == NULL)
+        return -1;
+    else {
+        while(!feof(fichero)){
+            fscanf(fichero,"%d",&nuevo.dni);
+            fscanf(fichero," %[^\n]",nuevo.nombre);
+            fscanf(fichero," %[^\n]",nuevo.telefono);
+            fscanf(fichero,"%f",&nuevo.monto_vendido);
+            fscanf(fichero,"%d",&nuevo.cant_vendido);
+            fscanf(fichero," %[^\n]",nuevo.canal_venta);
+            a_mayuscula(nuevo.nombre);
+            /*
+            if(tipo_orden == 1)
+                resultado_alta = alta_lso(lista, nuevo);
+            else
+                resultado_alta = alta_lsobb(lista, nuevo);
+            if (resultado_alta == -1){
+                fclose(fichero);
+                break;
+            }
+            if (resultado_alta == 0){
+                repetidos ++;
+            }
+            indice ++;
+        }
+    }
+
+    *cargados = indice;
+    *cant_repetidos = repetidos;
+    fclose(fichero);
+
+    if(resultado_alta == -1){
+        return 0;
+    }
+    return 1;
+}*/
